f1b02fac11f1319f74c1598f81f08433b8aa943c
Max van der Stam <max@vanderstam.nl>
2018-08-22 09:39:34 +0200

    Update polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,7 +4,16 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload top &
+    MONITOR=$m polybar --reload bottom &
+  done
+else
+#  polybar --reload top &
+  polybar --reload bottom &
+fi
+
+echo "Bars launched..."
